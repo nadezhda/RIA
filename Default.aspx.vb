55919470154dc4d7c57ab16a67e4de1373d211ac
Heba Dairieh <hnrd1g13@soton.ac.uk>
2014-03-31 16:42:52 +0100

    Initial app folder
    extra gitignore

---
+++ Default.aspx.vb
@@ -0,0 +1,17 @@
+﻿
+Option Explicit On
+Option Strict On
+
+Imports System
+Imports System.Web
+Imports System.Web.UI
+
+
+Partial Public Class [Default]
+	Inherits System.Web.UI.Page
+	
+	Public Sub button1Clicked(ByVal sender As Object, ByVal args As EventArgs)
+		button1.Text = "You clicked me"
+	End Sub
+End Class
+
