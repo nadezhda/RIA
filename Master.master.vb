e17054e80b773f73963e2bb58f6f564d0f4555c9
Heba Dairieh <hnrd1g13@soton.ac.uk>
2014-03-31 17:26:24 +0100

    Created Initial table view in default.aspx

--- Master.master.vb
+++ Master.master.vb
@@ -7,9 +7,7 @@ Imports System.Web
 Imports System.Web.UI
 
 
-Partial Public Class [Default]
-	Inherits System.Web.UI.Page
-	
-
+Partial Public Class Master
+	Inherits System.Web.UI.MasterPage
 End Class
 
